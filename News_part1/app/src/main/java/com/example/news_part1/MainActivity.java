75507de909aacae014a48c2025471a14eb057303
fang5796 <chiawei9059@gmail.com>
2019-09-04 14:13:42 +0800

    news_part1

---
+++ News_part1/app/src/main/java/com/example/news_part1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.news_part1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToKeyword(View view) {
+        Intent intent =  new Intent(MainActivity.this, KeywordActivity.class);
+        startActivity(intent);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
